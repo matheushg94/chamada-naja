ce6e07b332c1c27fce28653c4aba994202ece01a
Matheus <mat.hen.gra@gmail.com>
2024-04-23 15:26:59 -0300

    updates

--- script/visualizar.js
+++ script/visualizar.js
@@ -1,29 +1,125 @@
 const formVisualizar = document.querySelector('#form-visualizar');
 
-const getDate = () => {
+const getDate = (turma) => {
     const todayDate = new Date();
     const month = todayDate.getMonth();
     const year = todayDate.getFullYear();
-    const date = new Date(year, month, 1);
+    let diaNumero;
+    let diaDaSemana;
+        
+    for (let i = 1; i < 8; i++) {
+        const date = new Date(year, month, i);
+        const weekDay = date.getDay();
+        const monthDay = date.getDate();
 
-    console.log(date);
+        if (turma.toLowerCase().startsWith('seg')) {
+            if (weekDay === 1) {
+                diaNumero = monthDay;
+                diaDaSemana = 'segunda';
+                break;
+            }
+            if (weekDay === 3) {
+                diaNumero = monthDay;
+                diaDaSemana = 'quarta';
+                break;
+            }
+        } else {
+            if (weekDay === 2) {
+                diaNumero = monthDay;
+                diaDaSemana = 'terça';
+                break;
+            }
+            if (weekDay === 4) {
+                diaNumero = monthDay;
+                diaDaSemana = 'quinta';
+                break;
+            }
+        }
+    }
+
+    return {diaNumero, diaDaSemana}
+}
+
+const getListaDeDias = (diaNumero, diaDaSemana) => {
+    let lista = [diaNumero];
+    let contador = diaNumero;
+
+    switch (diaDaSemana) {
+        case 'segunda':
+            for (let i = 0; i < 9; i++) {
+                if (i % 2 === 0) {
+                    contador+= 2;
+                    lista.push(contador)
+                } else {
+                    contador+= 5;
+                    lista.push(contador);
+                }
+            }
+            break;
+
+        case 'terça':
+            for (let i = 0; i < 9; i++) {
+                if (i % 2 === 0) {
+                    contador+= 2;
+                    lista.push(contador)
+                } else {
+                    contador+= 5;
+                    lista.push(contador);
+                }
+            }
+            break;
+
+        case 'quarta':
+            for (let i = 0; i < 9; i++) {
+                if (i % 2 === 0) {
+                    contador+= 5;
+                    lista.push(contador)
+                } else {
+                    contador+= 2;
+                    lista.push(contador);
+                }
+            }
+            break;
+
+        case 'quinta':
+            for (let i = 0; i < 9; i++) {
+                if (i % 2 === 0) {
+                    contador+= 5;
+                    lista.push(contador)
+                } else {
+                    contador+= 2;
+                    lista.push(contador);
+                }
+            }
+            break;
+    
+        default:
+            break;
+    }
+
+    return lista;
 }
 
 const createTable = (turma) => {
     const tb = document.createElement('table');
     const headerRow = document.createElement('tr');
-    const numero = document.createElement('th');
-    numero.innerText = 'N°';
-    const nome = document.createElement('th');
-    nome.innerText = 'NOME';
-    const dias = document.createElement('th');
-    dias.innerText = 'DIAS';
-    headerRow.appendChild(numero);
-    headerRow.appendChild(nome);
-    headerRow.appendChild(dias);
+    const thNumero = document.createElement('th');
+    thNumero.innerText = 'N°';
+    const thNome = document.createElement('th');
+    thNome.innerText = 'NOME';
+    headerRow.appendChild(thNumero);
+    headerRow.appendChild(thNome);
+    
+    const {diaNumero, diaDaSemana} = getDate(turma);
+    const listaDeDias = getListaDeDias(diaNumero, diaDaSemana);
+    listaDeDias.forEach(dia => {
+        const celulaDia = document.createElement('th');
+        celulaDia.innerText = dia;
+        headerRow.appendChild(celulaDia);
+    })
+
     tb.appendChild(headerRow);
 
-    getDate();
 
     const listaAlunos = JSON.parse(localStorage.getItem('lista'));
         listaAlunos.forEach(aluno => {
@@ -33,11 +129,13 @@ const createTable = (turma) => {
                 num.innerText = `1`;
                 const nome = document.createElement('td');
                 nome.innerText = `${aluno.nome}`;
-                const dias = document.createElement('td');
-                dias.innerText = `${aluno.turma}`;
                 row.appendChild(num);
                 row.appendChild(nome);
-                row.appendChild(dias);
+                listaDeDias.forEach(dia => {
+                    const blankCell = document.createElement('td');
+                    blankCell.innerText = ' ';
+                    row.appendChild(blankCell);
+                })
                 tb.appendChild(row);
             }
         });
